96aaa730552e71006f3398819e92de78101eaa69
Boluwaduro00 <adebajobolu@gmail.com>
2025-08-27 13:45:23 +0100

    update dynamic product page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,28 +1,48 @@
+"use client";
+
+import { fetchProductsByID } from "@/store/slice/ProductSlice";
+import { AppDispatch, RootState } from "@/store/store";
 import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ProductDetails() {
+  const { productId } = useParams();
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { selectedProduct } = useSelector((state: RootState) => state.products);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(fetchProductsByID(Number(productId)));
+    }
+  }, [dispatch, productId]);
+
   return (
     <div className="container mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-2 gap-10">
       <div className="flex flex-col items-center">
         <div className="w-full h-[400px] relative">
           <Image
-            src="/phone.png"
+            src={selectedProduct?.image || "/phone.png"}
             alt="Product Image"
             fill
+            sizes="200px"
             className="object-contain rounded-xl border"
           />
         </div>
-    
+
         <div className="flex gap-3 mt-4">
           <Image
-            src="/phone.png"
+            src={selectedProduct?.image || "/phone.png"}
             alt="Thumbnail"
             width={80}
             height={80}
             className="object-contain border rounded-lg cursor-pointer"
           />
           <Image
-            src="/phone.png"
+            src={selectedProduct?.image || "/phone.png"}
             alt="Thumbnail"
             width={80}
             height={80}
@@ -31,20 +51,20 @@ export default function ProductDetails() {
         </div>
       </div>
 
-      
       <div>
-        <h1 className="text-3xl font-bold mb-3">Product Title</h1>
-        <p className="text-xl font-semibold text-gray-700 mb-3">$120.00</p>
+        <h1 className="text-3xl font-bold mb-3">{selectedProduct?.title}</h1>
+        <p className="text-xl font-semibold text-gray-700 mb-3">
+          ${selectedProduct?.price}
+        </p>
 
-        
         <div className="flex items-center gap-2 mb-4">
-          ⭐⭐⭐⭐☆ <span className="text-sm text-gray-500">(120 reviews)</span>
+          ⭐⭐⭐⭐☆{" "}
+          <span className="text-sm text-gray-500">
+            ({selectedProduct?.rating.count} reviews)
+          </span>
         </div>
 
-      
-        <p className="text-gray-600 mb-6">
-          This is a sample product description. We’ll pull this from the API later.
-        </p>
+        <p className="text-gray-600 mb-6">{selectedProduct?.description}</p>
 
         {/* Buttons */}
         <div className="flex gap-4">
